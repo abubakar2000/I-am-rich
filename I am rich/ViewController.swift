6956e793c3e6313053396cd67f043890b2ceff6b
Abubakar Mughal <64458681+abubakar2000@users.noreply.github.com>
2022-01-31 20:22:52 +0500

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Abubakar Mughal on 31/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
